75ed7611074edb6745823ebec90b14eb460e7fae
sicalo330 <132423242+sicalo330@users.noreply.github.com>
2024-02-02 14:36:20 -0500

    Agregar enrutamiento, recordar arreglar el problema de los links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,33 @@ import {MatGridListModule} from '@angular/material/grid-list';
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from "@angular/material/button";
 
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import 'hammerjs';
+
+//Esto de aquí hace parte de un modulo generado por el comnado ng g module nombre, sirve para el enrutamiento de angular
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component'
 
 //Esto es un servicio de angular por lo tanto se debe poner en providers y no en imports
 import { DishService } from './services/dish.service';
+
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+
+
+export const routes:Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'about', component:AboutComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'', redirectTo:'/home',pathMatch:'full'},
+]
+
 
 //Todas las importaciones que haga se deben exportar en la sección llamada imports
 
@@ -24,7 +42,12 @@ import { DishService } from './services/dish.service';
   declarations: [
     AppComponent,
     MenuComponent,
-    DishdetailComponent
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -35,7 +58,8 @@ import { DishService } from './services/dish.service';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [ DishService],
   bootstrap: [AppComponent]
